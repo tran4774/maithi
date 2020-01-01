f7ea4e5845d17a4c442aa6ee829c4d9d6e48ddcd
tran4774 <59108692+tran4774@users.noreply.github.com>
2020-01-01 12:20:13 +0700

    Update and rename Đếm từ có 2 nguyên âm.cpp to All of Strings.cpp

--- All of Strings.cpp
+++ All of Strings.cpp
@@ -5,16 +5,27 @@
 #include <math.h>
 #include <ctype.h>
 using namespace std;
-int main()
-{
-    char str[50];
-    
-    do
-    {
-        cout << "Moi nhap chuoi: ";
-        cin.getline(str, 50);
-        cout << "Chuoi vua nhap la: ";
-        cout << str << endl;
+//Hàm tính tổng (hoặc trung bình cộng) các số nguyên trong CHUỖI
+double tongtrongchuoi(char str[]) {
+	int len = strlen(str);
+	int i = len - 1;
+	int tong = 0, num = 0, dem = 0;
+	while (i >= 0) {
+		while (i >= 0 && !isdigit(str[i])) i--;
+		num = 0;
+		int hang = 1;
+		while (i >= 0 && isdigit(str[i])) {
+			num += hang * (str[i] - 48);
+			hang *= 10;
+			i--;
+		}
+		tong += num;
+		dem++;                              //Tính tổng thì bỏ dòng này
+	}
+	return (double)tong / (double)dem;      //Tính tổng thỉ return tong;
+}
+//Hàm đếm từ có 2 nguyên âm liền trong chuỗi 
+void demtu2na(char str[]) {
         char na[] = "AEIOUYaeiouy";
         int sotu01 = 0; int dem2 = 0;
         int len = strlen(str);
@@ -32,6 +43,18 @@ int main()
            dem2 = sotu01 - dem;
         }
         cout << "So tu co chua it nhat hai nguyen am dung canh nhau trong mang la: "<<dem2 << endl;
+}
+int main()
+{
+    char str[50];
+    
+    do
+    {
+        cout << "Moi nhap chuoi: ";
+        cin.getline(str, 50);
+        cout << "Chuoi vua nhap la: ";
+        cout << str << endl;
+
     } 
     while (_getch() != 27);
 }
