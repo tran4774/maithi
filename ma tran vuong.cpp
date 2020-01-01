50de07d549114df5dc25156e36225778d37b24b5
FaithLopper <59404450+FaithLopper@users.noreply.github.com>
2020-01-01 14:39:06 +0700

    Update ma tran vuong.cpp

--- ma tran vuong.cpp
+++ ma tran vuong.cpp
@@ -57,7 +57,7 @@ bool ktmatrandonvi(int a[][SIZE], int n)
 
 bool ktmatrantamgiactren(int a[][SIZE], int n)
 {
-	for (int i = 1; i < n-1; i++)
+	for (int i = 0; i < n; i++)
 		for (int j = 0; j < i; j++)
 			if (a[i][j] != 0) return false;
 
