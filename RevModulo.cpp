6df2940e461025ae6931f5c0aaca28d70d5228c2
Tran Nguyen <tran4774@gmail.com>
2020-04-16 09:44:40 +0700

    Cách 2
    add cách 2 tính mod^-1

--- RevModulo.cpp
+++ RevModulo.cpp
@@ -11,22 +11,40 @@ using namespace std;
 
 
 
-int revmodulo(int n, int m);
+int revmodulo(long long n, int m);
 int main() {
-	int n, m;
+	long long n;
+	int m;
 	cout << "Nhap n: ";
 	cin >> n;
-	//cout << "Nhap m: ";
-	//cin >> m;
+	cout << "Nhap m: ";
+	cin >> m;
 	int start = clock();
-	cout << revmodulo(n, 1000000007) << endl;
+	int mod = revmodulo(n, m);
+	cout << mod << endl;
 	cout << clock() - start << " ms" << endl;
 	return 0;
 }
-int revmodulo(int k, int m) {
-	if (k == 1)
-		return k;
-	vector<int> x = { k, m };
+int revmodulo(long long k, int m) {
+	if (k % m == 1)
+		return 1;
+	long long  k2 = k, m2 = m;
+	int a1 = 1, a2 = 0, a3;
+	int r = k2 % m2;
+	while (r > 1) {
+		a3 = a1 - (a2 * int(k2 / m2));
+		r = k2 % m2;
+		k2 = m2;
+		m2 = r;
+		a1 = a2;
+		a2 = a3;
+	}
+	if (r != 1)
+		return -1;
+	while (a2 < 0) 
+		a2 += m;
+	return a2;
+	/*vector<long long> x = { k, m };
 	vector<int> a = { 1, 0 };
 	vector<int> b = { 0, 1 };
 	vector<int> y = { 0 };
@@ -39,5 +57,5 @@ int revmodulo(int k, int m) {
 		b.push_back(b[i - 2] - (b[i - 1] * y[i - 1]));
 	}
 	while (*(a.end() - 1) < 0) *(a.end() - 1) += m;
-	return *(a.end() - 1);
+	return *(a.end() - 1);*/
 }
